4f9908f54d4e52e21b5e4c1a0d4d593dafb94321
Neha Kumari <neha.gemini92@gmail.com>
2020-11-05 00:48:55 +0530

    correction

--- jobs/app.py
+++ jobs/app.py
@@ -15,12 +15,8 @@ def open_connection():
     return connection
 
 
-def execute_sql():
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
-    sql = ''
-    values = ()
-    commit = False
-    single = False
     cursor = connection.execute(sql, values)
     if commit:
         results = connection.commit()
